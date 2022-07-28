a2e331d3d9dab1e205e8ddbef8cf918f480ef925
Pablo <pablogarcirodriguez@gmail.com>
2022-07-28 09:38:38 +0200

    clase 17: getters and setters

--- assets/index.js
+++ assets/index.js
@@ -2,7 +2,12 @@ import MediaPlayer from './MediaPlayer.js';
 import AutoPlay from './plugins/AutoPlay.js';
 
 const video = document.querySelector("video");
-const player = new MediaPlayer({element: video, plugins:[new AutoPlay()] });  //con esta linea creamos una nueva instancia de MediaPlayer
+const player = new MediaPlayer({
+    element: video, 
+    plugins:[
+        new AutoPlay()
+    ], 
+});  //con esta linea creamos una nueva instancia de MediaPlayer
 
 const button = document.querySelector("button");
 const buttonMute = document.getElementById("mute");
