1f6cc49b2f078fa8e476fbc4c3a7d6a9d867629b
Pablo <pablogarcirodriguez@gmail.com>
2022-08-04 17:52:25 +0200

    Clase 25: Tipos Primitivos TypeScript

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -1,7 +1,41 @@
-function add(a: number, b: number){
-    return a + b
+// Boolean
+let muted: boolean = true;
+muted = false;
+
+//Number
+let numero = 6
+let numerador: number = 6
+let denominador:number = numero
+let result = numerador/denominador
+
+//String
+let nombre: string = 'Richard'
+let frase = `Me llamo ${nombre}`
+
+//Array
+let people: string[] = [];
+people = ['Isable', 'Nicole', 'Raul'];
+//people.push('900')
+let peopleAndNumbers: Array < string | number > = []
+peopleAndNumbers.push('Ricardo')
+peopleAndNumbers.push(500)
+
+//Enum
+enum Color {
+    Rojo = 'Rojo',
+    Verde = 'Verde',
+    Azul = 'Azul',
+    Amarillo = 'Amarillo'
 }
 
-const sum = add(4,5);
+let colorFavorito: Color = Color.Verde;
+console.log(`Mi color favorito es ${colorFavorito}`)
 
-console.log(sum);
+//Any
+let comodin: any = 'Joker';
+comodin = { type: 'wildcard'}
+comodin = 7;
+
+//Object
+
+let carta: object = { type: 'wildcard'}
