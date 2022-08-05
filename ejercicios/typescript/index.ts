b4661fd7687eb2a5219c410414f8df736fad5711
Pablo <pablogarcirodriguez@gmail.com>
2022-08-05 11:40:52 +0200

    Clase 27: Interfaces en TypeScript

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -1,66 +1,101 @@
-// Boolean
-let muted: boolean = true;
-muted = false;
+// // Boolean
+// let muted: boolean = true;
+// muted = false;
 
-//Number
-let numero = 6
-let numerador: number = 6
-let denominador:number = numero
-let result = numerador/denominador
+// //Number
+// let numero = 6
+// let numerador: number = 6
+// let denominador:number = numero
+// let result = numerador/denominador
 
-//String
-let nombre: string = 'Richard'
-let frase = `Me llamo ${nombre}`
+// //String
+// let nombre: string = 'Richard'
+// let frase = `Me llamo ${nombre}`
 
-//Array
-let people: string[] = [];
-people = ['Isable', 'Nicole', 'Raul'];
-//people.push('900')
-let peopleAndNumbers: Array < string | number > = []
-peopleAndNumbers.push('Ricardo')
-peopleAndNumbers.push(500)
+// //Array
+// let people: string[] = [];
+// people = ['Isable', 'Nicole', 'Raul'];
+// //people.push('900')
+// let peopleAndNumbers: Array < string | number > = []
+// peopleAndNumbers.push('Ricardo')
+// peopleAndNumbers.push(500)
 
-//Enum
-enum Color {
+// //Enum
+// enum Color {
+//     Rojo = 'Rojo',
+//     Verde = 'Verde',
+//     Azul = 'Azul',
+//     Amarillo = 'Amarillo'
+// }
+
+// let colorFavorito: Color = Color.Verde;
+// console.log(`Mi color favorito es ${colorFavorito}`)
+
+// //Any
+// let comodin: any = 'Joker';
+// comodin = { type: 'wildcard'}
+// comodin = 7;
+
+// //Object
+
+// let carta: object = { type: 'wildcard'}
+
+// //Funciones
+
+// function add(a: number, b: number): number{
+//     return a + b;
+// }
+
+// const sum = add(4, 5)
+
+// function createAdder(a: number): (number)=>number{
+//     return function(b: number){
+//         return b + a;
+//     }
+// }
+
+// const addFour = createAdder(4);
+// const fourPlusSix = addFour(6);
+
+
+// function fullName(firstName: string, lastName: string = 'Smith'): string{
+//     return `${firstName} ${lastName}`
+// };
+
+// const richard = fullName('Richard');
+// console.log(richard)
+
+
+
+//Interfaces
+
+enum Color  {
     Rojo = 'Rojo',
-    Verde = 'Verde',
-    Azul = 'Azul',
-    Amarillo = 'Amarillo'
+    Verde = 'Verde'
 }
 
-let colorFavorito: Color = Color.Verde;
-console.log(`Mi color favorito es ${colorFavorito}`)
-
-//Any
-let comodin: any = 'Joker';
-comodin = { type: 'wildcard'}
-comodin = 7;
-
-//Object
-
-let carta: object = { type: 'wildcard'}
-
-//Funciones
-
-function add(a: number, b: number): number{
-    return a + b;
+interface Rectangulo {
+    ancho: number,
+    alto: number
+    color?: Color
 }
 
-const sum = add(4, 5)
-
-function createAdder(a: number): (number)=>number{
-    return function(b: number){
-        return b + a;
-    }
+let rect: Rectangulo = {
+    ancho: 4,
+    alto: 6,
+    //color: Color.Rojo
 }
 
-const addFour = createAdder(4);
-const fourPlusSix = addFour(6);
+function area(r: Rectangulo): number {
+    return r.alto * r.ancho;
+}
 
+const areaRect = area(rect);
 
-function fullName(firstName: string, lastName: string = 'Smith'): string{
-    return `${firstName} ${lastName}`
-};
+console.log(areaRect);
 
-const richard = fullName('Richard');
-console.log(richard)
+rect.toString = function() {
+    return `Un rectángulo ${this.color|| ''}`
+}
+
+console.log(rect.toString());
