795b5a819b1bd59e1e575de3601b57ba0ab08378
Pablo <pablogarcirodriguez@gmail.com>
2022-08-05 12:11:09 +0200

    Clase 28: Clases en TypeScript, aplicado al MediaPlayer

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,67 +1,68 @@
-function MediaPlayer(config){
-    this.media = config.element 
+class MediaPlayer {
+  constructor(config) {
+    this.media = config.element;
     /*con esta linea declaramos que el valor de media es el atributo que se le pasa a MediaPlayer al instanciarlo
-    En este caso el atributo que se le pasa al crear la instancia player es video, por lo que en player 
+    En este caso el atributo que se le pasa al crear la instancia player es video, por lo que en player
     this.media siempre será igual que video, pues es el argumento que se le ha pasado*/
     this.plugins = config.plugins || [];
     this._initPlugins();
   }
+  _initPlugins() {
+    const player = {
+      play: () => this.play(),
+      pause: () => this.pause(),
+      media: this.media,
+      get muted() {
+        return this.media.muted;
+      },
 
-MediaPlayer.prototype._initPlugins = function(){
-  const player = {
-    play: ()=> this.play(),
-    pause: ()=> this.pause(),
-    media: this.media,
-    get muted(){
-      return this.media.muted;
-    },
-
-    set muted(value){
-      this.media.muted = value;
-  },
-};
+      set muted(value) {
+        this.media.muted = value;
+      },
+    };
 
 
-  this.plugins.forEach(plugin => {
-    plugin.run(player);
-  });
-}
-
-MediaPlayer.prototype.play = function() {   //con esto definimos la funcion play de la clase MediaPlayer
+    this.plugins.forEach(plugin => {
+      plugin.run(player);
+    });
+  }
+  play() {
     this.media.play();
-  } 
-
-MediaPlayer.prototype.pause = function(){ //igual que la anterior función pero para la funcion pause
+  }
+  pause() {
     this.media.pause();
   }
-
-  
   /* La siguiente funcion comprueba si el video está pausado
-  a través de la propiedad paused, en funcion de si devuelve true o false
-  ejecutará play o pause.*/  
-  
-  MediaPlayer.prototype.togglePlay = function(){
-      if(this.media.paused){
-          this.play();
-        }else{
-        this.pause();
-        }
-    }    
-
-  MediaPlayer.prototype.mute = function(){
+    a través de la propiedad paused, en funcion de si devuelve true o false
+    ejecutará play o pause.*/
+  togglePlay() {
+    if (this.media.paused) {
+      this.play();
+    } else {
+      this.pause();
+    }
+  }
+  mute() {
     this.media.muted = true;
   }
-
-  MediaPlayer.prototype.unmute = function(){
+  unmute() {
     this.media.muted = false;
   }
-
-  MediaPlayer.prototype.toggleMute = function(){
-    if(this.media.muted){
+  toggleMute() {
+    if (this.media.muted) {
       this.unmute();
-    }else{
+    } else {
       this.mute();
     }
   }
+}
+
+
+
+
+  
+
+
+
 
 export default MediaPlayer;
--- assets/plugins/AutoPause.ts
+++ assets/plugins/AutoPause.ts
@@ -1,4 +1,8 @@
+import MediaPlayer from "../MediaPlayer";
+
 class AutoPause{
+    private threshold: number;
+    player: MediaPlayer;
     constructor(){
         this.threshold = 0.25
         this.handleIntersection = this.handleIntersection.bind(this);
@@ -16,7 +20,7 @@ class AutoPause{
     }
 
 
-    handleIntersection(entries) {
+    private handleIntersection(entries: IntersectionObserverEntry[]) {
         const entry = entries[0];
         const isVisible = entry.intersectionRatio >= this.threshold
         if (isVisible) {
@@ -26,7 +30,7 @@ class AutoPause{
         }
     }
 
-    handleVisibilityChange(){
+    private handleVisibilityChange(){
         const isVisible = document.visibilityState === 'visible'
         if(isVisible){
             this.player.play()
