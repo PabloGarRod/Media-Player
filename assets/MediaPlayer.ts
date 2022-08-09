b375740663cf57e0e2ccc97ab89ebe303eb180c7
Pablo <pablogarcirodriguez@gmail.com>
2022-08-09 18:17:13 +0200

    Clases 39 y 40: Implementacion del Plugin Ads

--- assets/MediaPlayer.ts
+++ assets/MediaPlayer.ts
@@ -1,14 +1,24 @@
 class MediaPlayer {
   media: HTMLMediaElement;
   plugins: Array<any>;
+  container: HTMLElement;
   constructor(config) {
     this.media = config.element;
     /*con esta linea declaramos que el valor de media es el atributo que se le pasa a MediaPlayer al instanciarlo
     En este caso el atributo que se le pasa al crear la instancia player es video, por lo que en player
     this.media siempre será igual que video, pues es el argumento que se le ha pasado*/
     this.plugins = config.plugins || [];
+    this.initPlayer();
     this.initPlugins();
   }
+
+  initPlayer(){
+    this.container = document.createElement('div')
+    this.container.style.position = 'relative'
+    this.media.parentNode.insertBefore(this.container, this.media)
+    this.container.appendChild(this.media)
+  }
+
   private initPlugins() {
     this.plugins.forEach(plugin => {
       plugin.run(this);
--- assets/index.ts
+++ assets/index.ts
@@ -1,13 +1,16 @@
 import MediaPlayer from './MediaPlayer';
 import AutoPlay from './plugins/AutoPlay';
 import AutoPause from './plugins/AutoPause';
+import Ads from './plugins/Ads';
+
 
 const video = document.querySelector("video");
 const player = new MediaPlayer({
     element: video, 
     plugins:[
         new AutoPlay(),
-        new AutoPause()
+        new AutoPause(),
+        new Ads()
     ], 
 });  //con esta linea creamos una nueva instancia de MediaPlayer
 
